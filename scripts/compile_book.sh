a0566c40fec227abac6fe918e16c2e37ee1c9ea7
gllmar <gllm@artificiel.org>
2021-01-16 20:20:23 -0500

    +copie ./resources -> _book

--- scripts/compile_book.sh
+++ scripts/compile_book.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo $SCRIPTPATH
+RESOURCES_DIR="resources" 
+OUTPUT_DIR="docs"
 
 cd "$SCRIPTPATH"
+echo "copy $RESOURCES_DIR to "$OUTPUT_DIR"/"$RESOURCES_DIR""
 cd ../
+echo "copy resources to $OUTPUT_DIR"
+mkdir -p "$OUTPUT_DIR"/"$RESOURCES_DIR"
+cp -r "$RESOURCES_DIR"/* "$OUTPUT_DIR"/"$RESOURCES_DIR"
+echo "compile the book"
+
 Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
 Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_book")'
 Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::epub_book")'
--- scripts/serve_book.sh
+++ scripts/serve_book.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo $SCRIPTPATH
+OUTPUT_DIR="_book"
+RESOURCES_DIR="resources" 
 
 cd "$SCRIPTPATH"
+echo "copy $RESOURCES_DIR to "$OUTPUT_DIR"/"$RESOURCES_DIR""
 cd ../
+mkdir -p "$OUTPUT_DIR"/"$RESOURCES_DIR"
+cp -r "$RESOURCES_DIR"/* "$OUTPUT_DIR"/"$RESOURCES_DIR"
+echo "Serve the book"
 Rscript -e 'bookdown::serve_book(dir = ".", output_dir = "_book", preview = TRUE, in_session = TRUE, quiet = FALSE)'
